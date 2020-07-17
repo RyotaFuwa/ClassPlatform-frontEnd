8a5bb47269602f57b30fa369a25c0c7b9f6c330d
rytoafuwa <ryota.fuwa.1428@gmail.com>
2020-07-17 22:47:23 +0900

    made blob draggable

--- src/components/PageBlock/Separator.js
+++ src/components/PageBlock/Separator.js
@@ -1,10 +1,28 @@
 import React, {Component} from 'react';
-import './PageBlob.css';
+import './Blob.css';
 
 const Separator = props => {
     return (
-      <div className='separator' />
-    )
+      <div className='separator'
+           onDragEnter={(e) => {
+             let style = e.target.style;
+             style.height = '100px';
+             style.backgroundColor = '#C8C8CD';
+           }}
+           onDragLeave={(e) => {
+             let style = e.target.style;
+             style.height = '25px';
+             style.backgroundColor = null;
+           }}
+           onDragOver={e => e.preventDefault()}
+           onDrop={(e) => {
+             let style = e.target.style;
+             style.height = '25px';
+             style.backgroundColor = null;
+             props.onDrop();
+           }}
+      />
+)
 }
 
 export default Separator;
